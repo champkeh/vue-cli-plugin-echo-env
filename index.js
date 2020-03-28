9c3ffffc149ba8ff0498d3e7d805ad809ef381f3
champkeh <champion.ge@gmail.com>
2020-03-28 13:07:43 +0800

    打印帮助信息，输出NODE_ENV的值

--- index.js
+++ index.js
@@ -6,35 +6,45 @@ module.exports = (api, projectOptions) => {
         // 获取要检查的变量前缀，默认为 VUE_APP_ (客户端变量)
         _prefix = args.prefix || _prefix;
 
-        console.log(`current prefix: ${chalk.red(_prefix)}\n`);
+        printHelp();
+
+        console.log(`${chalk.gray('current environment variable prefix:')} ${chalk.red(_prefix)}`);
+        console.log(`NODE_ENV=${chalk.green(process.env.NODE_ENV)}\n`);
 
         const varNames = Object.keys(process.env).filter(name => name.startsWith(_prefix));
         if (varNames.length === 0) {
             console.log(chalk.red('Not matched any env variables!'));
         }
         const maxLength = Math.max(...varNames.map(name => name.length));
-        const result = varNames.map(name => ({ name, value: process.env[name] }));
-        result.forEach(({ name, value }) => {
+        const result = varNames.map(name => ({name, value: process.env[name]}));
+        result.forEach(({name, value}) => {
             formatEntry(name, maxLength + 4, value)
         })
     })
 };
 
-function formatEntry (name, maxLength, value) {
+function formatEntry(name, maxLength, value) {
     console.log(`${colorName(pad(name, maxLength))}${colorValue(value)}`)
 }
 
-function pad (val, maxLength, char = ' ') {
+function pad(val, maxLength, char = ' ') {
     if (val.length >= maxLength) {
         return val
     }
     return val + char.repeat(maxLength - val.length)
 }
 
-function colorName (name) {
+function colorName(name) {
     return chalk.red(name.substr(0, _prefix.length)) + chalk.cyan(name.substr(_prefix.length))
 }
 
-function colorValue (value) {
+function colorValue(value) {
     return chalk.green(value)
+}
+
+function printHelp() {
+    console.log('>> ' + chalk.blue('You can use ') + chalk.yellow('--prefix') + chalk.blue(' to custom variable\'s prefix'));
+    console.log('>> ' + chalk.white('Examples:'));
+    console.log('>> ' + chalk.cyan('vue-cli-service inspect-env --prefix VUE_CLI_'));
+    console.log('\n')
 }
